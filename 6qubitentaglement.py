5441e759590987b142132cf666f778f2dd11504b
Atul Jha <atuljha370@gmail.com>
2026-09-16 01:39:19 +0530

    Implement 6-qubit entanglement circuit using Qiskit

---
+++ 6qubitentaglement.py
@@ -0,0 +1,26 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+
+circuit=QuantumCircuit(6,6)
+
+circuit.h(0)
+
+circuit.cx(0,1)
+circuit.cx(1,2)
+circuit.cx(2,3)
+circuit.cx(3,4)
+circuit.cx(4,5)
+circuit.measure(0,0)
+circuit.measure(1,1)
+circuit.measure(2,2)
+circuit.measure(3,3)
+circuit.measure(4,4)
+circuit.measure(5,5)
+
+simulator=AerSimualtor()
+result=simulator.run(circuit).result()
+counts=result.get_counts()
+print("\n---6-QUBIT CHAIN REACTION COMPLETE")
+print(counts)
+print("----------------------\n")
+print(circuit.draw())
